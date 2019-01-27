1cf6be7c83fc66f4b3629f8acf1639d7e1a6a5a9
ribozz <ribozz@gmail.com>
2019-01-27 18:56:24 +0200

    Added package data

--- setup.py
+++ setup.py
@@ -3,6 +3,36 @@ from glob import glob
 
 from setuptools import setup, find_packages
 
+
+def find_package_data(*allowed_extensions):
+    package_data = {}
+
+    for ext in allowed_extensions:
+        for file in glob(f'**/*.{ext}', recursive=True):
+            pos = -1
+            parts = file.split('/')
+            while True:
+                prefix = '/'.join(parts[0:pos])
+                package = '.'.join(parts[0:pos])
+                data_file = '/'.join(parts[pos:])
+
+                if prefix == '':
+                    break
+
+                if os.path.exists(os.path.join(prefix, '__init__.py')):
+                    break
+
+                pos -= 1
+
+            if prefix != '':
+                if package not in package_data:
+                    package_data[package] = []
+
+                package_data[package].append(data_file)
+
+    return package_data
+
+
 setup(
     name='zmei-gen-flutter',
     version='0.1.0',
@@ -13,6 +43,8 @@ setup(
     author_email='ribozz@gmail.com',
     description='FLutter generator for Zmei',
     long_description='',
+
+    package_data=find_package_data('g4', 'tpl', 'html'),
 
     install_requires=[
         "zmei-cli>=2.1.1",
